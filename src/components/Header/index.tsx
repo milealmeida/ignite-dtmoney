c0f88871f7f1ba949a6170ef99d8f5e86f951ec1
Milena <milenaalmeidadev@hotmail.com>
2021-05-04 10:28:07 -0400

    Componentização

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { Container, Content } from './style';
+
+import logoImg from '../../assets/logo.svg';
+
+export function Header() {
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money"/>
+                <button type="button">
+                    Nova transação
+                </button>
+            </Content>
+        </Container>
+    );
+}
